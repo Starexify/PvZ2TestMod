690fbdca655f6a1ef2287e72d0dc6bed2981b8d6
ImVeryBad <nightbear843@yahoo.com>
2026-04-19 10:48:52 +0200

    Add android logging system from android c headers.

---
+++ src/log.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const main = @import("root");
+const androidLog = @cImport({
+  @cInclude("android/log.h");
+});
+
+const TAG = if (@hasDecl(main, "LIB_TAG")) main.LIB_TAG else "DEFAULT_MOD";
+
+pub fn log(prio: c_int, comptime fmt: []const u8, args: anytype) void {
+  var buf: [512]u8 = undefined;
+  const formatted = std.fmt.bufPrintZ(&buf, fmt, args) catch "Log overflow";
+  _ = androidLog.__android_log_print(prio, TAG, "%s", formatted.ptr);
+}
+
+pub fn info(comptime fmt: []const u8, args: anytype) void { log(androidLog.ANDROID_LOG_INFO, fmt, args); }
--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
-
+const log = @import("log.zig");
 const sh = @cImport({
   @cInclude("shadowhook.h");
 });
 
-extern fn __android_log_print(prio: i32, tag: [*:0]const u8, fmt: [*:0]const u8, ...) i32;
-const ANDROID_LOG_INFO = 4;
-const LIB_TAG = "PVZ2_MOD";
+pub const LIB_TAG = "PVZ2_MOD";
 
 export fn mod_main() callconv(.c) void {
   // _ = sh.shadowhook_init(sh.SHADOWHOOK_MODE_SHARED, true);
-  _ = __android_log_print(ANDROID_LOG_INFO, LIB_TAG, sh.shadowhook_get_version());
+  log.info("Initialized Mod: {s}", .{LIB_TAG});
+  log.info("ShadowHook Version: {s}", .{sh.shadowhook_get_version()});
 }
 
 export const init_array_pointer: *const fn () callconv(.c) void linksection(".init_array") = &mod_main;
@@ -18,5 +17,5 @@ export const init_array_pointer: *const fn () callconv(.c) void linksection(".in
 export fn JNI_OnLoad(vm: *anyopaque, _: *anyopaque) callconv(.c) i32 {
   _ = vm;
   // _ = __android_log_print(ANDROID_LOG_INFO, LIB_TAG, "JNI OnLoad");
-    return 0x00010006;
+  return 0x00010006;
 }
