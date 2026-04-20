e66517bafc107f53fa24d3445e562650764b807e
ImVeryBad <nightbear843@yahoo.com>
2026-04-21 00:52:29 +0200

    Changed RtClass and RtObject to be closer to the .so file.

--- src/popcap/RtClass.zig
+++ src/popcap/RtClass.zig
@@ -5,14 +5,32 @@ pub const RtClass = extern struct {
   _padding: [SIZE - 8]u8, // offset 0x08
 
   pub const offsets = struct {
-    pub const ctor = 0x00f382e8; // RtClass::RtClass
-    pub const new   = 0x02349248; // new RtClass() or RtClass->new
-    pub const t_GetType = 0x02349434; // RtClass::t_GetType
+    pub const RtClass_Definition = 0x02dc7bd0; // RtObject::RtObject_Definition
 
-    pub const GetType = 0x02349364; // RtClass::GetType
+    pub const ctor      = 0x00f382e8; // RtClass::RtClass
+    pub const new       = 0x02349248; // new RtClass() or RtClass->new
+
+    // VTable Function Addr
+    pub const GetType   = 0x02349364; // RtClass::GetType
+    pub const t_GetType = 0x02349434; // RtClass::t_GetType
+    pub const Dtor      = 0x02349434; // RtClass::~RtClass
+    pub const Destroy   = 0x02349434; // RtClass::Destroy
+    pub const Func4     = 0x02349298; // RtObject::Func4
+    pub const Func5     = 0x023492cc; // RtObject::Func5
+    pub const Func6     = 0x02349f7c; // RtClass::Func6
+    pub const Func7     = 0x02349be4; // RtClass::Func7
+    pub const Func8     = 0x02349c20; // RtClass::Func8
   };
 
   pub const VTable = extern struct {
     GetType: *const fn (*anyopaque) callconv(.c) *RtClass,
+    Func1:   *const fn (*anyopaque) callconv(.c) bool,
+    Dtor:    *const fn (*anyopaque) callconv(.c) void,
+    Destroy: *const fn (*anyopaque) callconv(.c) void,
+    Func4:   *const fn (*anyopaque) callconv(.c) void,
+    Func5:   *const fn (*anyopaque) callconv(.c) i64,
+    Func6:   *const fn (*anyopaque) callconv(.c) void,
+    Func7:   *const fn (*anyopaque) callconv(.c) bool,
+    Func8:   *const fn (*anyopaque) callconv(.c) void,
   };
 };
--- src/popcap/RtObject.zig
+++ src/popcap/RtObject.zig
@@ -8,26 +8,26 @@ pub const RtObject = extern struct {
   pub const offsets = struct {
     pub const RtObject_Definition = 0x02dc7bd0; // RtObject::RtObject_Definition
 
-    pub const ctor                = 0x012abf00; // RtObject::RtObject
-    pub const new                 = 0x00f365f4; // new RtObject() or RtObject->new
-    pub const s_GetType           = 0x02349160; // RtObject::s_GetType
+    pub const ctor      = 0x012abf00; // RtObject::RtObject
+    pub const new       = 0x00f365f4; // new RtObject() or RtObject->new
+    pub const s_GetType = 0x02349160; // RtObject::s_GetType
 
     // VTable Function Addr
-    pub const GetType             = 0x023491d4; // RtObject::GetType
-    pub const Func1               = 0x02349290; // RtObject::Func1
-    pub const Func2               = 0x0234a24c; // RtObject::Func2
-    pub const Destroy             = 0x02df5028; // RtObject::Destroy
-    pub const Func4               = 0x02349298; // RtObject::Func4
-    pub const Func5               = 0x023492cc; // RtObject::Func5
-    pub const Func6               = 0x023492d4; // RtObject::Func6
+    pub const GetType   = 0x023491d4; // RtObject::GetType
+    pub const Func1     = 0x02349290; // RtObject::Func1
+    pub const Dtor      = 0x0234a24c; // RtObject::~RtObject
+    pub const Destroy   = 0x02df5028; // RtObject::Destroy
+    pub const Func4     = 0x02349298; // RtObject::Func4
+    pub const Func5     = 0x023492cc; // RtObject::Func5
+    pub const Func6     = 0x023492d4; // RtObject::Func6
 
-    pub const Dat1                = 0x02d53bd8; // RtObject::Dat1
+    pub const Dat1      = 0x02d53bd8; // RtObject::Dat1
   };
 
   pub const VTable = extern struct {
     GetType: *const fn (*anyopaque) callconv(.c) *RtClass,
     Func1:   *const fn (*anyopaque) callconv(.c) bool,
-    Func2:   *const fn (*anyopaque) callconv(.c) void,
+    Dtor:    *const fn (*anyopaque) callconv(.c) void,
     Destroy: *const fn (*anyopaque) callconv(.c) void,
     Func4:   *const fn (*anyopaque) callconv(.c) void,
     Func5:   *const fn (*anyopaque) callconv(.c) i64,
